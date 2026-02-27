93de51090d9ad9d74a3f2ca0ccdbf39ff4bd2081
Shannah <shannahs2605@gmail.com>
2026-02-27 09:52:39 -0500

    added another column for time-created

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,16 @@
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, DateTime
 from app.db.database import Base
+import datetime
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String, nullable=False)
+    username = Column(String(50), unique=True, nullable=False, index=True)
+    email = Column(String(100), unique=True, index=True)
+    password = Column(String(100), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+
+
+
