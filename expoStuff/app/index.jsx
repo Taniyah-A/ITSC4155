72c0010c70ff042d6fcea874f67f2c5b8ac194f8
Ini Ogunsanya <156601535+IniOgunsanya@users.noreply.github.com>
2026-04-10 11:51:05 -0400

    design changes, fixed feeback logic

--- expoStuff/app/index.jsx
+++ expoStuff/app/index.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import React, { useEffect, useState, useRef } from "react";
 import { mockQuestions } from "../src/data/mockQuestions";
-import { questionDb } from "../src/data/buddy_question";
+//import { questionDb } from "../src/data/buddy_question";
 
 //import { bg1 } from "../expoStuff/assets/images/bg1.jpg";
 
@@ -20,7 +20,7 @@ export default function QuestionsScreen() {
 
   const question = mockQuestions[questionIndex]; //change to use actual db
   //using database ahhh
-  const dbQuestion = questionDb[questionIndex];
+  //const dbQuestion = questionDb[questionIndex];
   const { width } = Dimensions.get("window");
 
   const handleSelect = (choice) => {
@@ -39,9 +39,9 @@ export default function QuestionsScreen() {
     setShowFeedback(false);
   };
 
+  const isCorrect = selected === question.correct_answer;
   const isWrong = showFeedback && !isCorrect;
   const isRight = showFeedback && isCorrect;
-  const isCorrect = selected === question.correct_answer;
 
   const ProgressBar = ({ percent }) => {
     const widthAnim = useRef(new Animated.Value(0)).current;
@@ -125,24 +125,31 @@ export default function QuestionsScreen() {
           {showFeedback && (
   <View
     style={[
-      styles.feedback,
-      isRight && styles.correctFeedback,
-      isWrong && styles.incorrectFeedback,
+      isRight && styles.feedbackCorrect,
+      isWrong && styles.feedbackIncorrect,
     ]}
   >
     {isRight && (
-      <Text style={styles.feedbackText}>Excellent!</Text>
+      <View>
+        <Text style={styles.feedbackTextCorrect}>Excellent!</Text>
+        <TouchableOpacity style={styles.nextBtnCorrect} onPress={handleNext}>
+          <Text style={styles.nextText}>Next Question</Text>
+        </TouchableOpacity>
+    </View>
     )}
 
     {isWrong && (
-      <Text style={{fontSize: 16, color: "#B00020", textAlign: "center"}}>
-        Correct answer: {question.correct_answer}
-      </Text>
+      <View>
+        <Text style={styles.feedbackTextIncorrect}>
+          Correct answer: {question.correct_answer}
+        </Text>
+        <TouchableOpacity style={styles.nextBtnIncorrect} onPress={handleNext}>
+          <Text style={styles.nextText}>Next Question</Text>
+        </TouchableOpacity>
+    </View>
+
     )}
 
-    <TouchableOpacity style={styles.nextBtn} onPress={handleNext}>
-      <Text style={styles.nextText}>Next Question</Text>
-    </TouchableOpacity>
   </View>
 )}
         </>
@@ -160,81 +167,46 @@ const styles = StyleSheet.create({
   },
 
   question: { //deisgn for the box the question
-    // flex: 2,
-    // backgroundColor: "#FBF6F2",
-    // borderRadius: 28,
-    // padding: 28,
-    // alignItems: "center",
-    // borderWidth: 2,
-    // borderColor: "#f9e1ce",
-    // elevation: 6,
-    // height: 80,
-    // justifyContent: "center",
-    // gap: 12,
-    // fontSize: 20,
-    // textAlign: "center",
-    // marginTop: 40,
-    fontSize: 20,
+    elevation: 6,
+    height: 100,
+    gap: 12,
+    marginTop: 40,
+    fontSize: 30,
     fontWeight: "600",
     textAlign: "center",
     marginVertical: 20,
+    color: "#4f2300",
   },
 
-  /*choice: {
-    flex: 3,
-    backgroundColor: "#FBF6F2",
+  choicesGrid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    gap: 12,
+  },
+
+  choice: {
+    width: "48%",
+    height: 120,
+    backgroundColor: "#fdf6f1",
     borderRadius: 18,
     borderWidth: 2.5,
     borderColor: "#f9e1ce",
-    alignItems: "center",
     justifyContent: "center",
-    height: 20,
-    shadowColor: "#8B7ECC",
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.1,
-    shadowRadius: 6,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOpacity: 0.05,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 2 },
     elevation: 3,
   },
 
   choiceText: {
-    fontSize: 24,
-    textAlign: "center",
-  },*/
+    fontSize: 20,
+    fontWeight: "500",
+  },
 
-  choicesGrid: {
-  flexDirection: "row",
-  flexWrap: "wrap",
-  justifyContent: "space-between",
-  gap: 12,
-},
-
-choice: {
-  width: "48%",
-  height: 120,
-  backgroundColor: "#FFF",
-  borderRadius: 16,
-  borderWidth: 2,
-  borderColor: "#E5E5E5",
-  justifyContent: "center",
-  alignItems: "center",
-
-  shadowColor: "#000",
-  shadowOpacity: 0.05,
-  shadowRadius: 5,
-  shadowOffset: { width: 0, height: 2 },
-  elevation: 3,
-},
-
-choiceText: {
-  fontSize: 18,
-  fontWeight: "500",
-},
-
-  feedback: {
-    // flex: 4,
-    // alignItems: "center",
-    // justifyContent: "center",
-    // gap: 20,
+  feedbackCorrect: {
     position: "absolute",
     bottom: 0,
     left: 0,
@@ -243,6 +215,33 @@ choiceText: {
     padding: 20,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+    height: 150,
+  },
+
+  feedbackTextCorrect: {
+    fontSize: 24,
+    fontWeight: "600",
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#58CC02",
+  },
+
+  feedbackIncorrect: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#FDC4C4",
+    padding: 20,
+    height: 150,
+  },
+
+  feedbackTextIncorrect: {
+    fontSize: 24,
+    fontWeight: "600",
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#D13A3A",
   },
   
   correctChoice: {
@@ -267,32 +266,20 @@ choiceText: {
 
   incorrect: {
     //design for the bottom thing saying the right answer
-    //marginTop: 20,
-    // flex: 3,
-    // fontSize: 22,
-    // textAlign: "center",
-    // color: "#A72D2D",
-    // opacity: 0.5,
-    // borderRadius: 8,
-    // width: "100%",
-    // backgroundColor: "#FDC4C4",
-    //height: "100%",
     fontSize: 16,
     color: "#B00020",
     textAlign: "center",
     marginBottom: 10,
   },
 
-  nextBtn: {
-    // marginTop: 50,
-    // backgroundColor: "#5AF0F1",
-    // padding: 12,
-    // borderRadius: 8,
-    // //flex: 2,
-    // justifyContent: "flex-end",
-    // opacity: 1.0,
-    // alignSelf: "flex-end",
+  nextBtnCorrect: {
     backgroundColor: "#58CC02",
+    paddingVertical: 14,
+    borderRadius: 12,
+  },
+
+  nextBtnIncorrect: {
+    backgroundColor: "#D13A3A",
     paddingVertical: 14,
     borderRadius: 12,
   },
@@ -327,6 +314,8 @@ choiceText: {
     height: "100%",
     backgroundColor: "#FFD166",
     borderRadius: 5,
+    borderColor: "#e7c7ad",
+    borderWidth: 1,
   },
 
   topBar: {
