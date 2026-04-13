0e6cf75737ea11ff98a810ab9fde705e81f6e12a
Ini Ogunsanya <156601535+IniOgunsanya@users.noreply.github.com>
2026-04-13 08:34:05 -0400

    added backgrounds and changed the logic to use try again

--- expoStuff/app/index.jsx
+++ expoStuff/app/index.jsx
@@ -1,27 +1,28 @@
+import { useEffect, useRef, useState } from "react";
 import {
+  Animated,
+  Dimensions,
+  ImageBackground,
   StyleSheet,
   Text,
-  View,
   TouchableOpacity,
-  ImageBackground,
-  Dimensions,
-  Animated,
+  View,
 } from "react-native";
-import React, { useEffect, useState, useRef } from "react";
 import { mockQuestions } from "../src/data/mockQuestions";
 //import { questionDb } from "../src/data/buddy_question";
-
-//import { bg1 } from "../expoStuff/assets/images/bg1.jpg";
 
 export default function QuestionsScreen() {
   const [questionIndex, setQuestionIndex] = useState(0); // change to use the actual db
   const [selected, setSelected] = useState(null);
   const [showFeedback, setShowFeedback] = useState(false);
-
+  const backgrounds = [
+    require("../assets/imgs/bg1.jpg"),
+    require("../assets/imgs/bg2.jpg"),
+  ];
+  const [randomBg, setRandomBg] = useState(
+    backgrounds[Math.floor(Math.random() * backgrounds.length)],
+  );
   const question = mockQuestions[questionIndex]; //change to use actual db
-  //using database ahhh
-  //const dbQuestion = questionDb[questionIndex];
-  const { width } = Dimensions.get("window");
 
   const handleSelect = (choice) => {
     setSelected(choice);
@@ -34,9 +35,16 @@ export default function QuestionsScreen() {
     setShowFeedback(false);
   };
 
-  const handleHome = () => { //come back
+  const handleHome = () => {
+    //come back
     setSelected(null);
     setShowFeedback(false);
+  };
+
+  const tryAgainFunc = () => {
+    setSelected(null);
+    setShowFeedback(false);
+    setQuestionIndex(questionIndex);
   };
 
   const isCorrect = selected === question.correct_answer;
@@ -72,110 +80,119 @@ export default function QuestionsScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      {question && (
-        <>
-        <View style={styles.topBar}>
-          <ProgressBar
-            style={styles.progressTracker}
-            percent={((questionIndex + 1) / mockQuestions.length) * 100}
-          />
-          <TouchableOpacity style={styles.homeBtn} onPress={handleHome}>
+    <ImageBackground source={randomBg} style={styles.screenBackground}>
+      <View style={styles.container}>
+        {question && (
+          <>
+            <View style={styles.topBar}>
+              <ProgressBar
+                style={styles.progressTracker}
+                percent={((questionIndex + 1) / mockQuestions.length) * 100}
+              />
+              {/*goes back to homescreen -> todo#2*/}
+              <TouchableOpacity style={styles.homeBtn} onPress={handleHome}>
                 <Text style={{ fontSize: 20 }}>X</Text>
-          </TouchableOpacity>
-          
-        </View>
-        <Text style={styles.question}>{question.question}</Text>
-          {/*background
-          <ImageBackground
-            source={ bg1}
-            style={styles.mapBackground}
-            resizeMode="cover"
-          />*/}
-
-          {/*//shows the choices displays correct answer regardless -> todo#2*/}
-          <View style={styles.choicesGrid}>
-            {question.choices.map((choice, i) => {
-            const isSelected = selected === choice;
-            const choiceStyles = [
-              styles.choice,
-              isSelected && styles.selected,
-              showFeedback &&
-                isCorrect &&
-                choice === question.correct_answer &&
-                styles.correctChoice,
-              showFeedback &&
-                isSelected &&
-                !isCorrect &&
-                styles.incorrectChoice,
-            ];
-            return (
-              <TouchableOpacity
-                key={i}
-                style={choiceStyles}
-                onPress={() => handleSelect(choice)}
-                disabled={showFeedback}
-              >
-                <Text style={styles.choiceText}>{choice}</Text>
               </TouchableOpacity>
-            );
-          })}
-          </View>
+            </View>
+            <View style={styles.question}>
+              <Text style={styles.questionText}>{question.question}</Text>
+            </View>
 
-          {showFeedback && (
-  <View
-    style={[
-      isRight && styles.feedbackCorrect,
-      isWrong && styles.feedbackIncorrect,
-    ]}
-  >
-    {isRight && (
-      <View>
-        <Text style={styles.feedbackTextCorrect}>Excellent!</Text>
-        <TouchableOpacity style={styles.nextBtnCorrect} onPress={handleNext}>
-          <Text style={styles.nextText}>Next Question</Text>
-        </TouchableOpacity>
-    </View>
-    )}
+            <View style={styles.choicesGrid}>
+              {question.choices.map((choice, i) => {
+                const isSelected = selected === choice;
+                const choiceStyles = [
+                  styles.choice,
+                  isSelected && styles.selected,
+                  showFeedback &&
+                    isCorrect &&
+                    choice === question.correct_answer &&
+                    styles.correctChoice,
+                  showFeedback &&
+                    isSelected &&
+                    !isCorrect &&
+                    styles.incorrectChoice,
+                ];
+                return (
+                  <TouchableOpacity
+                    key={i}
+                    style={choiceStyles}
+                    onPress={() => handleSelect(choice)}
+                    disabled={showFeedback}
+                  >
+                    <Text style={styles.choiceText}>{choice}</Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
 
-    {isWrong && (
-      <View>
-        <Text style={styles.feedbackTextIncorrect}>
-          Correct answer: {question.correct_answer}
-        </Text>
-        <TouchableOpacity style={styles.nextBtnIncorrect} onPress={handleNext}>
-          <Text style={styles.nextText}>Next Question</Text>
-        </TouchableOpacity>
-    </View>
+            {showFeedback && (
+              <View
+                style={[
+                  isRight && styles.feedbackCorrect,
+                  isWrong && styles.feedbackIncorrect,
+                ]}
+              >
+                {isRight && (
+                  <View>
+                    <Text style={styles.feedbackTextCorrect}>Excellent!</Text>
+                    <TouchableOpacity
+                      style={styles.nextBtnCorrect}
+                      onPress={handleNext}
+                    >
+                      <Text style={styles.nextText}>Next Question</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
 
-    )}
-
-  </View>
-)}
-        </>
-      )}
-    </View>
+                {isWrong && (
+                  <View>
+                    <TouchableOpacity
+                      style={styles.nextBtnIncorrect}
+                      onPress={tryAgainFunc}
+                    >
+                      <Text style={styles.nextText}>Try again!</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
+              </View>
+            )}
+          </>
+        )}
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FBF6F2", //doesnt matter - should be a picture
+    //backgroundColor: "#FBF6F2", //doesnt matter - should be a picture
     paddingHorizontal: 20,
     paddingTop: 50,
   },
 
-  question: { //deisgn for the box the question
+  question: {
+    //deisgn for the box the question
     elevation: 6,
-    height: 100,
-    gap: 12,
-    marginTop: 40,
-    fontSize: 30,
+    height: 150,
+    gap: 10,
+    marginTop: 80,
     fontWeight: "600",
     textAlign: "center",
     marginVertical: 20,
+    backgroundColor: "#FFF9F3",
+    borderRadius: 18,
+    borderColor: "#f9e1ce",
+    borderWidth: 2.5,
+  },
+
+  questionText: {
     color: "#4f2300",
+    fontSize: 30,
+    fontWeight: "500",
+    textAlign: "center",
+    paddingTop: 40,
   },
 
   choicesGrid: {
@@ -243,7 +260,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: "#D13A3A",
   },
-  
+
   correctChoice: {
     //highlights the right answer
     backgroundColor: "#C8F499",
@@ -279,6 +296,7 @@ const styles = StyleSheet.create({
   },
 
   nextBtnIncorrect: {
+    marginTop: 30,
     backgroundColor: "#D13A3A",
     paddingVertical: 14,
     borderRadius: 12,
@@ -291,8 +309,8 @@ const styles = StyleSheet.create({
   },
 
   homeBtn: {
-    width:36,
-    paddingHorizontal: 10, 
+    width: 36,
+    paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 8,
     backgroundColor: "#f9e1ce",
@@ -322,5 +340,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 10,
+  },
+
+  screenBackground: {
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    blurRadius: 100,
   },
 });
